c16214612db4d129fc9846edfe6520261e8330e0
Christian Soltenborn <christian.soltenborn@gmail.com>
2024-12-06 08:11:49 +0100

    Blatt 6: A1 c)

--- done/Blatt6A1.java
+++ done/Blatt6A1.java
@@ -10,9 +10,27 @@ public class Blatt6A1 {
         return new SinglyLinkedList(new ListNode(value, list.root()));
     }
 
+    static String toString(final SinglyLinkedList list) {
+        return toString(list.root());
+    }
+
+    static String toString(final ListNode current) {
+        if (current == null) {
+            return "";
+        }
+        String head = String.valueOf(current.value);
+        String tail = toString(current.next());
+        if ("".equals(tail)) {
+            return head;
+        }
+        return head + ", " + tail;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList(new ListNode(42, null));
         list = addFirst(list, 23);
+
+        System.out.println(toString(list));
     }
-    
+
 }
