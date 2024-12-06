e428e74da3a404d1bcdc12722f2003b65eedc8a3
Christian Soltenborn <christian.soltenborn@gmail.com>
2024-12-06 08:10:03 +0100

    Blatt 6: A1 b)

--- done/Blatt6A1.java
+++ done/Blatt6A1.java
@@ -6,5 +6,13 @@ public class Blatt6A1 {
 
     public record SinglyLinkedList(ListNode root) {}
 
+    static SinglyLinkedList addFirst(final SinglyLinkedList list, final int value) {
+        return new SinglyLinkedList(new ListNode(value, list.root()));
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList(new ListNode(42, null));
+        list = addFirst(list, 23);
+    }
     
 }
